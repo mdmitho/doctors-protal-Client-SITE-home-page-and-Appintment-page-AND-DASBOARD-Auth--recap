23fd1ab0e9a62f5f9c0ead659dcbfde869355be3
Hossen <mdmitho199911@gmail.com>
2022-05-13 23:04:35 +0600

    Create booking object

---
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -0,0 +1,90 @@
+import { format } from "date-fns";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+
+
+const BookingModal = ({ date, treatment, setTreatment }) => {
+  const [user, loading, error] = useAuthState(auth);
+  const { _id, name, slots } = treatment;
+  const formattedDate = format(date,'PP');
+  
+
+  const handleBooking = (event) => {
+    event.preventDefault();
+    const slot = event.target.slot.value;
+    // console.log(slot);
+
+    const booking ={
+      treatmentId : _id,
+      treatment:name,
+      date: formattedDate,
+      slot,
+      patient: user.email,
+      patientName : user.displayName,
+      phone:event.target.phone.value
+    }
+
+    setTreatment(null);
+  };
+
+  return (
+    <div>
+      <input type="checkbox" id="Booking-modal" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <label htmlFor="Booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">
+            ✕
+          </label>
+          <h3 className="font-bold text-lg">Booking for: {name}</h3>
+          <form
+            className="grid grid-cols-1 gap-3 justify-items-center mt-2"
+            onSubmit={handleBooking}
+          >
+            <input
+              type="text"
+              disabled
+              value={format(date, "PP")}
+              className="input input-bordered w-full max-w-xs"
+            />
+            <select name="slot" className="select select-bordered w-full max-w-xs">
+              {slots.map((slot,index) => (
+                <option
+                key={index}
+                 value={slot}
+                 >{slot}</option>
+              ))}
+            </select>
+            <input
+              type="text"
+              name="name"
+              disabled 
+              value={user?.displayName ||''}
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="email"
+              name="email"
+             disabled
+             value={user?.email||''}
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              name="phone"
+              placeholder="Phone Number"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="submit"
+              placeholder="Submit"
+              className="btn btn-secondary w-full max-w-xs"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingModal;

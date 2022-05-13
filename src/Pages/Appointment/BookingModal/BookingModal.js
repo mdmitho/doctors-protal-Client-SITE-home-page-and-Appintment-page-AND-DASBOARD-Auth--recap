b891cbf966af72867719d58c8138a9f0f38de5a2
Hossen <mdmitho199911@gmail.com>
2022-05-14 01:55:49 +0600

    React Query to update slots automatically

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -5,51 +5,47 @@ import toast from "react-hot-toast";
 import auth from "../../../firebase.init";
 
 
-const BookingModal = ({ date, treatment, setTreatment }) => {
+const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
   const [user, loading, error] = useAuthState(auth);
   const { _id, name, slots } = treatment;
-  const formattedDate = format(date,'PP');
-  
+  const formattedDate = format(date, "PP");
 
   const handleBooking = (event) => {
     event.preventDefault();
     const slot = event.target.slot.value;
     // console.log(slot);
 
-    const booking ={
-      treatmentId : _id,
-      treatment:name,
+    const booking = {
+      treatmentId: _id,
+      treatment: name,
       date: formattedDate,
       slot,
       patient: user.email,
-      patientName : user.displayName,
-      phone:event.target.phone.value
-    }
-    fetch("http://localhost:5000/booking",{
-      method: 'POST',
-      headers:{
-        'content-type': 'application/json'
+      patientName: user.displayName,
+      phone: event.target.phone.value,
+    };
+    fetch("http://localhost:5000/booking", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
       },
-      body: JSON.stringify(booking)
+      body: JSON.stringify(booking),
     })
-    .then(res => res.json())
-    .then(data => {
-      //to close the modal
-      console.log(data);
-      if(data.success){
-        toast.successst(`Appointment is set, ${formattedDate} at ${slot}`);
-      }
-      else{
-        toast.error(`Already have and appointment on ${data.booking?.date} at ${data.booking?.slot}`);
-      }
-
-
-
-      //to close the modal
-      setTreatment(null);
-    })
-
-
+      .then((res) => res.json())
+      .then((data) => {
+        //to close the modal
+        console.log(data);
+        if (data.success) {
+          toast.success(`Appointment is set, ${formattedDate} at ${slot}`);
+        } else {
+          toast.error(
+            `Already have and appointment on ${data.booking?.date} at ${data.booking?.slot}`
+          );
+        }
+refetch()
+        //to close the modal
+        setTreatment(null);
+      });
   };
 
   return (
@@ -57,7 +53,10 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
       <input type="checkbox" id="Booking-modal" className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-          <label htmlFor="Booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">
+          <label
+            htmlFor="Booking-modal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
             ✕
           </label>
           <h3 className="font-bold text-lg">Booking for: {name}</h3>
@@ -72,25 +71,24 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
               className="input input-bordered w-full max-w-xs"
             />
             <select name="slot" className="select select-bordered w-full max-w-xs">
-              {slots.map((slot,index) => (
-                <option
-                key={index}
-                 value={slot}
-                 >{slot}</option>
+              {slots.map((slot, index) => (
+                <option key={index} value={slot}>
+                  {slot}
+                </option>
               ))}
             </select>
             <input
               type="text"
               name="name"
-              disabled 
-              value={user?.displayName ||''}
+              disabled
+              value={user?.displayName || ""}
               className="input input-bordered w-full max-w-xs"
             />
             <input
               type="email"
               name="email"
-             disabled
-             value={user?.email||''}
+              disabled
+              value={user?.email || ""}
               className="input input-bordered w-full max-w-xs"
             />
             <input
